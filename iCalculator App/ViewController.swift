5da7ce76ce4f05cf02f64691f9c06edc41f26816
Bakhrom Usmanov <bakhromusmanov01@gmail.com>
2024-06-30 13:53:39 +0500

    Initial Commit

---
+++ iCalculator App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iCalculator App
+//
+//  Created by MacBook Air on 30/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
